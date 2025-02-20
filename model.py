8314b0c8abfc15b86a5c82fb00b1fb6b3e2c5ca6
Kevin <94776797+Kepinnnn@users.noreply.github.com>
2025-02-20 20:40:49 +0700

    Delete model.py

--- model.py
+++
@@ -1,8 +0,0 @@
-from transformers import pipeline
-
-classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
-
-def classify_expense(description):
-    labels = ["Makanan", "Transportasi", "Belanja", "Tagihan", "Hiburan"]
-    result = classifier(description, labels)
-    return result['labels'][0]  # Mengambil label dengan skor tertinggi
